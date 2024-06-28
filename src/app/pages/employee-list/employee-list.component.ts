6a3e0a4eac00249febe566812fc27a8e8d6044c7
Pratham-1a19 <pradpat1918@gmail.com>
2024-06-28 11:42:35 +0530

    Data integrated in employee-list component

--- src/app/pages/employee-list/employee-list.component.ts
+++ src/app/pages/employee-list/employee-list.component.ts
@@ -8,25 +8,53 @@ import { Router } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { AddEmployeeComponent } from './add-employee/add-employee.component';
 import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { CompanyService } from '../../core/services/company.service';
 import { CustomizerSettingsService } from '../../customizer-settings/customizer-settings.service';
+import { CoreModule } from '../../core/core.module';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-employee-list',
   standalone: true,
-  imports: [RouterLink, MatCardModule, MatButtonModule, MatMenuModule, MatTooltipModule, MatIconModule],
+  imports: [RouterLink, MatCardModule, MatButtonModule, MatMenuModule, MatTooltipModule, MatIconModule,CoreModule,HttpClientModule,CommonModule],
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.scss'
 })
 export class EmployeeListComponent {
+  staffData: any = [];
+  staffDataTable: any = [];
+  collectionSize = 0;
+  filterEmployeeList: any = [];
+  paginateData: any = [];
+  page = 1;
+  pageSize = 50;
+
   constructor(
     private router: Router,
-    public themeService: CustomizerSettingsService
+    public themeService: CustomizerSettingsService,
+    private companyService: CompanyService
   ) {
     this.themeService.isToggled$.subscribe(isToggled => {
       this.isToggled = isToggled;
     });
+    this.getStaffDetails();
   }
 
+  getStaffDetails() {
+    this.companyService.getEmployeeDetailsData().subscribe((res: any) => {
+      this.staffDataTable = res.filter((staff: any) => staff.role !== 'companyAdmin');
+      for (let i = 0; i < this.staffDataTable.length; i++) {
+        this.staffDataTable[i].index = i + 1;
+      }
+      this.collectionSize = this.staffDataTable.length;
+      this.filterEmployeeList = [...this.staffDataTable];
+      this.getPagintaion();
+    })
+  }
+  getPagintaion() {
+    this.paginateData = this.filterEmployeeList.slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+  }
   // isToggled
   isToggled = false;
 
